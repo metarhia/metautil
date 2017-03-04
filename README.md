e736df5a80a158333bd56ce2c6967a2b49e97f8e
tshemsedinov <timur.shemsedinov@gmail.com>
2017-03-04 20:06:46 +0200

    Library structure

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Common
-Metarhia Common Library
+# Metarhia Common Library
+
+Namespace: `api.common` in Impress.
+Structure:
+  - Data structures manipulations: `/lib/data.js`
---
+++ common.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const data = require('./lib/data');
+
+const common = {};
+module.exports = common;
+
+Object.assign(
+  data // Data structures manipulations
+);
