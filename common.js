6ee1b202f4b31aa900b6b318309700473e234f62
Vlad Dziuba <dzyubavlad@gmail.com>
2017-06-20 17:19:04 +0300

    Implement requireEither function
    Refs: metarhia/jstp#216
    Fixes: #31
    Reviewed-By: Timur Shemsedinov <timur.shemsedinov@gmail.com>
    PR-URL: https://github.com/metarhia/common/pull/44

--- common.js
+++ common.js
@@ -13,13 +13,13 @@ const submodules = [
   'data', // Data structures manipulations
   'strings', // Strings utilities
   'time', // Data and Time functions
+  'functional', // Functional programming
   'misc', // Miscellaneous tools
   'units', // Units conversion
   'network', // Network utilities
   'id', // Kyes and identifiers
   'sort', // Sort compare functions
   'cache', // Cache (enhanced Map)
-  'functional' // Functional programming
 ];
 
 submodules
