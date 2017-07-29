3f3dcef89a755ba6735270c97015acaf931befaf
tshemsedinov <timur.shemsedinov@gmail.com>
2017-07-30 01:33:06 +0300

    Refactor emitter interface
    Closes: https://github.com/metarhia/common/issues/71
    Refs: https://github.com/metarhia/common/issues/70
    PR-URL: https://github.com/metarhia/common/pull/72

---
+++ test/events.js
@@ -0,0 +1,117 @@
+'use strict';
+
+const tap = require('tap');
+const events = require('events');
+const common = require('..');
+
+tap.test('emitter', (test) => {
+  const ee = common.emitter();
+  ee.on('name', () => {
+    test.end();
+  });
+  ee.emit('name');
+});
+
+tap.test('forward all events', (test) => {
+  test.plan(3);
+
+  const sourceEmitter = common.emitter();
+  const targetEmitter = common.emitter();
+
+  common.forwardEvents(sourceEmitter, targetEmitter);
+
+  targetEmitter.on('testEvent1', () => {
+    test.pass('event #1');
+  });
+
+  targetEmitter.on('testEvent2', () => {
+    test.pass('event #2');
+  });
+
+  targetEmitter.on('testEvent3', () => {
+    test.pass('event #3');
+  });
+
+  sourceEmitter.emit('testEvent1');
+  sourceEmitter.emit('testEvent2');
+  sourceEmitter.emit('testEvent3');
+});
+
+tap.test('forward all events by method', (test) => {
+  test.plan(3);
+
+  const sourceEmitter = common.emitter();
+  const targetEmitter = common.emitter();
+
+  sourceEmitter.forward(targetEmitter);
+
+  targetEmitter.on('testEvent1', () => {
+    test.pass('event #1');
+  });
+
+  targetEmitter.on('testEvent2', () => {
+    test.pass('event #2');
+  });
+
+  targetEmitter.on('testEvent3', () => {
+    test.pass('event #3');
+  });
+
+  sourceEmitter.emit('testEvent1');
+  sourceEmitter.emit('testEvent2');
+  sourceEmitter.emit('testEvent3');
+});
+
+tap.test('forward a single event', (test) => {
+  test.plan(1);
+
+  const sourceEventEmitter = new events.EventEmitter();
+  const targetEventEmitter = new events.EventEmitter();
+
+  common.forwardEvents(sourceEventEmitter, targetEventEmitter, 'testEvent');
+
+  targetEventEmitter.on('testEvent', () => {
+    test.pass('event handler must be called');
+  });
+
+  sourceEventEmitter.emit('testEvent');
+});
+
+tap.test('forward a single event under a new name', (test) => {
+  test.plan(1);
+
+  const sourceEventEmitter = new events.EventEmitter();
+  const targetEventEmitter = new events.EventEmitter();
+
+  common.forwardEvents(
+    sourceEventEmitter, targetEventEmitter, { testEvent: 'renamedEvent' }
+  );
+
+  targetEventEmitter.on('renamedEvent', () => {
+    test.pass('event handler must be called');
+  });
+
+  sourceEventEmitter.emit('testEvent');
+});
+
+tap.test('forward multiple events', (test) => {
+  test.plan(2);
+
+  const sourceEventEmitter = new events.EventEmitter();
+  const targetEventEmitter = new events.EventEmitter();
+
+  common.forwardEvents(
+    sourceEventEmitter, targetEventEmitter, ['event1', 'event2']
+  );
+
+  targetEventEmitter.on('event1', () => {
+    test.pass('first event handler must be called');
+  });
+
+  targetEventEmitter.on('event2', () => {
+    test.pass('second event handler must be called');
+  });
+
+  sourceEventEmitter.emit('event1');
+  sourceEventEmitter.emit('event2');
+});
