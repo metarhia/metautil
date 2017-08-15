343e98a60c4980a559a87f9d6824ee4a85c1635a
tshemsedinov <timur.shemsedinov@gmail.com>
2017-08-16 01:07:51 +0300

    Implement requireCallback and onceCallback
    PR-URL: https://github.com/metarhia/common/pull/95

--- test/callback.js
+++ test/callback.js
@@ -3,6 +3,25 @@
 const tap = require('tap');
 const common = require('..');
 
+tap.test('unsafeCallback', (test) => {
+  const callback = (...args) => {
+    test.strictSame(args, [1, 2, 3]);
+    test.end();
+  };
+  const args = [1, 2, 3, callback];
+  const cb = common.unsafeCallback(args);
+  test.strictSame(cb, callback);
+  cb(...args);
+});
+
+tap.test('unsafeCallback without callback', (test) => {
+  const args = [1, 2, 3];
+  const cb = common.unsafeCallback(args);
+  test.strictSame(args, [1, 2, 3]);
+  test.strictSame(cb, null);
+  test.end();
+});
+
 tap.test('safeCallback', (test) => {
   const callback = (...args) => {
     test.strictSame(args, [1, 2, 3]);
@@ -22,7 +41,7 @@ tap.test('safeCallback without callback', (test) => {
   test.end();
 });
 
-tap.test('safeCallback prevent callback twice', (test) => {
+tap.test('safeCallback', (test) => {
   const callback = (...args) => {
     test.strictSame(args, [1, 2, 3]);
     test.end();
@@ -31,26 +50,48 @@ tap.test('safeCallback prevent callback twice', (test) => {
   const wrappedCb = common.safeCallback(args);
   test.strictSame(typeof(wrappedCb), 'function');
   wrappedCb(...args);
-  wrappedCb(...args);
 });
 
-tap.test('unsafeCallback', (test) => {
+tap.test('safeCallback return emptiness', (test) => {
+  const args = [1, 2, 3];
+  const wrappedCb = common.safeCallback(args);
+  test.strictSame(wrappedCb, common.emptiness);
+  wrappedCb(...args);
+  test.end();
+});
+
+tap.test('onceCallback prevent callback twice', (test) => {
   const callback = (...args) => {
     test.strictSame(args, [1, 2, 3]);
     test.end();
   };
   const args = [1, 2, 3, callback];
-  const cb = common.unsafeCallback(args);
-  test.strictSame(cb, callback);
-  cb(...args);
+  const wrappedCb = common.onceCallback(args);
+  test.strictSame(typeof(wrappedCb), 'function');
+  wrappedCb(...args);
+  wrappedCb(...args);
 });
 
-tap.test('unsafeCallback without callback', (test) => {
+tap.test('requiredCallback', (test) => {
+  const callback = (...args) => {
+    test.strictSame(args, [1, 2, 3]);
+    test.end();
+  };
+  const args = [1, 2, 3, callback];
+  const wrappedCb = common.requiredCallback(args);
+  test.strictSame(typeof(wrappedCb), 'function');
+  wrappedCb(...args);
+});
+
+tap.test('requiredCallback raise', (test) => {
   const args = [1, 2, 3];
-  const cb = common.unsafeCallback(args);
-  test.strictSame(args, [1, 2, 3]);
-  test.strictSame(cb, null);
-  test.end();
+  try {
+    const wrappedCb = common.requiredCallback(args);
+    wrappedCb(...args);
+  } catch (err) {
+    test.strictSame(!!err, true);
+    test.end();
+  }
 });
 
 tap.test('once', (test) => {
