bae48e0faa8d128559a8623145552f30686dc85b
tshemsedinov <timur.shemsedinov@gmail.com>
2017-09-19 09:24:51 +0300

    Implement generateStorageKey
    PR-URL: https://github.com/metarhia/common/pull/100

---
+++ test/id.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const tap = require('tap');
+const common = require('..');
+
+tap.test('generateStorageKey', (test) => {
+  const key = common.generateStorageKey();
+  test.strictSame(Array.isArray(key), true);
+  test.strictSame(key.length, 3);
+  const [dir1, dir2, file] = key;
+  test.strictSame(dir1.length, 2);
+  test.strictSame(dir2.length, 2);
+  test.strictSame(file.length, 8);
+  test.strictSame(key.join('/').length, 14);
+  test.end();
+});
