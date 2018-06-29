77d2c96231a5596915cf0b6bd9cdd30d2ddd1515
tshemsedinov <timur.shemsedinov@gmail.com>
2018-06-29 19:53:32 +0300

    Optimize tests
    PR-URL: https://github.com/metarhia/common/pull/137

--- test/callback.js
+++ test/callback.js
@@ -18,45 +18,34 @@ api.metatests.test('unsafeCallback', (test) => {
 });
 
 api.metatests.test('unsafeCallback without callback', (test) => {
-  const args = [1, 2, 3];
+  const args = ['a', 'b', 'c'];
   const cb = api.common.unsafeCallback(args);
-  test.strictSame(args, [1, 2, 3]);
+  test.strictSame(args, ['a', 'b', 'c']);
   test.strictSame(cb, null);
   test.end();
 });
 
 api.metatests.test('safeCallback', (test) => {
   const callback = (...args) => {
-    test.strictSame(args, [1, 2, 3]);
+    test.strictSame(args, [10, 20, 30, 40, 50]);
     test.end();
   };
-  const args = [1, 2, 3, callback];
+  const args = [10, 20, 30, 40, 50, callback];
   const wrappedCb = api.common.safeCallback(args);
   test.strictSame(typeof(wrappedCb), 'function');
   wrappedCb(...args);
 });
 
 api.metatests.test('safeCallback without callback', (test) => {
-  const args = [1, 2, 3];
+  const args = [11, 22, 33];
   const wrappedCb = api.common.safeCallback(args);
-  test.strictSame(args, [1, 2, 3]);
+  test.strictSame(args, [11, 22, 33]);
   test.strictSame(wrappedCb, api.common.emptiness);
   test.end();
 });
 
-api.metatests.test('safeCallback', (test) => {
-  const callback = (...args) => {
-    test.strictSame(args, [1, 2, 3]);
-    test.end();
-  };
-  const args = [1, 2, 3, callback];
-  const wrappedCb = api.common.safeCallback(args);
-  test.strictSame(typeof(wrappedCb), 'function');
-  wrappedCb(...args);
-});
-
 api.metatests.test('safeCallback return emptiness', (test) => {
-  const args = [1, 2, 3];
+  const args = [3, 2, 1];
   const wrappedCb = api.common.safeCallback(args);
   test.strictSame(wrappedCb, api.common.emptiness);
   wrappedCb(...args);
@@ -65,10 +54,10 @@ api.metatests.test('safeCallback return emptiness', (test) => {
 
 api.metatests.test('onceCallback prevent callback twice', (test) => {
   const callback = (...args) => {
-    test.strictSame(args, [1, 2, 3]);
+    test.strictSame(args, ['A', 'B', 'C']);
     test.end();
   };
-  const args = [1, 2, 3, callback];
+  const args = ['A', 'B', 'C', callback];
   const wrappedCb = api.common.onceCallback(args);
   test.strictSame(typeof(wrappedCb), 'function');
   wrappedCb(...args);
@@ -77,17 +66,17 @@ api.metatests.test('onceCallback prevent callback twice', (test) => {
 
 api.metatests.test('requiredCallback', (test) => {
   const callback = (...args) => {
-    test.strictSame(args, [1, 2, 3]);
+    test.strictSame(args, [100, 200, 300]);
     test.end();
   };
-  const args = [1, 2, 3, callback];
+  const args = [100, 200, 300, callback];
   const wrappedCb = api.common.requiredCallback(args);
   test.strictSame(typeof(wrappedCb), 'function');
   wrappedCb(...args);
 });
 
 api.metatests.test('requiredCallback raise', (test) => {
-  const args = [1, 2, 3];
+  const args = [-1, -2, -3];
   try {
     const wrappedCb = api.common.requiredCallback(args);
     wrappedCb(...args);
