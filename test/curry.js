ffb561a0e608a38fbd82a2e4c4f3457aecd35c7d
tshemsedinov <timur.shemsedinov@gmail.com>
2018-07-14 23:56:20 +0300

    Fix curry test
    Refs: https://github.com/metarhia/common/pull/98
    PR-URL: https://github.com/metarhia/common/pull/139

--- test/curry.js
+++ test/curry.js
@@ -2,7 +2,7 @@
 
 api.metatests.test('curry(f)(1)(2)(3)', (test) => {
   const sum = (x, y, z) => (x + y + z);
-  const res = api.common.curry(sum, 1)(2)(3);
+  const res = api.common.curry(sum)(1)(2)(3);
   test.strictSame(res, 6);
   test.end();
 });
