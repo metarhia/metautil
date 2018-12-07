fb2de22edd749b051edb92fbe7f22f1c983c9e45
tshemsedinov <timur.shemsedinov@gmail.com>
2018-12-07 13:59:22 +0200

    Add token validation, deprecate SID functions
    Closes: https://github.com/metarhia/common/issues/234
    PR-URL: https://github.com/metarhia/common/pull/236

--- test/id.js
+++ test/id.js
@@ -5,6 +5,10 @@
 const metatests = require('metatests');
 const common = require('..');
 
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const secret = 'secret';
+const length = 64;
+
 const config = {
   characters: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
   secret: 'secret',
@@ -30,6 +34,23 @@ metatests.case('Common / id', { common }, {
         config.characters
       ),
       result => result.length === 4,
+    ],
+  ],
+  'common.validateToken': [
+    [secret, 'XFHczfaqXaaUmIcKfHNF9YAY4BRaMX5Z4Bx99rsB5UA499mTjmewlrWTKTCp77bc',  true],
+    [secret, 'XFHczfaqXaaUmIcKfHNF9YAY4BRaMX5Z4Bx99rsB5UA499mTjmewlrWTKTCp77bK', false],
+    [secret, '2XpU8oAewXwKJJSQeY0MByY403AyXprFdhB96zPFbpJxlBqHA3GfBYeLxgHxBhhZ', false],
+    [secret, 'WRONG-STRING',                                                     false],
+    [secret, '',                                                                 false],
+  ],
+  'common.generateToken': [
+    [secret, characters, length, token => common.validateToken(secret, token)],
+  ],
+  'common.crcToken': [
+    [
+      secret,
+      common.generateKey(length - 4, characters),
+      crc => crc.length === 4,
     ],
   ],
   'common.idToChunks': [
