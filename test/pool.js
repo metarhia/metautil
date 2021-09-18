f3c5ee0b75eec38ac556087dfb251719fd6f4e4f
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-09-18 21:43:57 +0300

    Implement Pool exclusive capture/release
    Closes: https://github.com/metarhia/metautil/issues/70
    
    PR-URL: https://github.com/metarhia/metautil/pull/71

--- test/pool.js
+++ test/pool.js
@@ -23,3 +23,22 @@ metatests.test('Pool: empty', (test) => {
   test.strictSame(pool.next(), null);
   test.end();
 });
+
+metatests.test('Pool: capture/release', (test) => {
+  const pool = new metautil.Pool();
+  const obj1 = {};
+  const obj2 = {};
+  const obj3 = {};
+  pool.add(obj1);
+  pool.add(obj2);
+  pool.add(obj3);
+  const item = pool.capture();
+  test.strictSame(item, obj1);
+  test.strictSame(pool.next(), obj2);
+  test.strictSame(pool.next(), obj3);
+  test.strictSame(pool.next(), obj2);
+  pool.release(item);
+  test.strictSame(pool.next(), obj3);
+  test.strictSame(pool.next(), obj1);
+  test.end();
+});
