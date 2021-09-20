47b9e673d9f7b7657323c0c3b31a8ea893e09722
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-09-20 12:26:50 +0300

    Extract asynchronity utilities to separate file
    PR-URL: https://github.com/metarhia/metautil/pull/79

--- metautil.js
+++ metautil.js
@@ -4,5 +4,6 @@ const utilities = require('./lib/utilities.js');
 const secutity = require('./lib/security.js');
 const semaphore = require('./lib/semaphore.js');
 const pool = require('./lib/pool.js');
+const async = require('./lib/async.js');
 
-module.exports = { ...utilities, ...secutity, ...semaphore, ...pool };
+module.exports = { ...utilities, ...secutity, ...semaphore, ...pool, ...async };
