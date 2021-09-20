cf52e878607547d53e2cbb50a87b9e2998a4a456
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-09-20 12:31:51 +0300

    Rename security.js to crypto.js
    PR-URL: https://github.com/metarhia/metautil/pull/79

--- metautil.js
+++ metautil.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const utilities = require('./lib/utilities.js');
-const secutity = require('./lib/security.js');
+const crypto = require('./lib/crypto.js');
+const async = require('./lib/async.js');
 const semaphore = require('./lib/semaphore.js');
 const pool = require('./lib/pool.js');
-const async = require('./lib/async.js');
 
-module.exports = { ...utilities, ...secutity, ...semaphore, ...pool, ...async };
+module.exports = { ...utilities, ...crypto, ...semaphore, ...pool, ...async };
