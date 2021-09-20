757c96508b4006d037679384afadc53431607846
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-09-20 12:22:05 +0300

    Extract Pool class to the separate file

--- metautil.js
+++ metautil.js
@@ -3,5 +3,6 @@
 const utilities = require('./lib/utilities.js');
 const secutity = require('./lib/security.js');
 const semaphore = require('./lib/semaphore.js');
+const pool = require('./lib/pool.js');
 
-module.exports = { ...utilities, ...secutity, ...semaphore };
+module.exports = { ...utilities, ...secutity, ...semaphore, ...pool };
