9b266f25c6bf943262aa34956184f76f53dec3dc
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-09-21 14:32:01 +0300

    Improve tests, add case to check pool order
    PR-URL: https://github.com/metarhia/metautil/pull/85

--- test/pool.js
+++ test/pool.js
@@ -5,12 +5,14 @@ const metautil = require('..');
 
 metatests.test('Pool: add/next', async (test) => {
   const pool = new metautil.Pool();
-  const obj1 = {};
-  const obj2 = {};
-  const obj3 = {};
+
+  const obj1 = { a: 1 };
   pool.add(obj1);
+  const obj2 = { a: 2 };
   pool.add(obj2);
+  const obj3 = { a: 3 };
   pool.add(obj3);
+
   test.strictSame(await pool.next(), obj1);
   test.strictSame(await pool.next(), obj2);
   test.strictSame(await pool.next(), obj3);
@@ -24,19 +26,22 @@ metatests.test('Pool: empty', async (test) => {
   test.end();
 });
 
-metatests.test('Pool: capture/release', async (test) => {
+metatests.test('Pool: capture/next', async (test) => {
   const pool = new metautil.Pool();
-  const obj1 = {};
-  const obj2 = {};
-  const obj3 = {};
+
+  const obj1 = { a: 1 };
   pool.add(obj1);
+  const obj2 = { a: 2 };
   pool.add(obj2);
+  const obj3 = { a: 3 };
   pool.add(obj3);
+
   const item = await pool.capture();
   test.strictSame(item, obj1);
   test.strictSame(await pool.next(), obj2);
   test.strictSame(await pool.next(), obj3);
   test.strictSame(await pool.next(), obj2);
+
   pool.release(item);
   try {
     pool.release(item);
@@ -48,20 +53,47 @@ metatests.test('Pool: capture/release', async (test) => {
   test.end();
 });
 
+metatests.test('Pool: capture/release', async (test) => {
+  const pool = new metautil.Pool();
+
+  const obj1 = { a: 1 };
+  pool.add(obj1);
+  const obj2 = { a: 2 };
+  pool.add(obj2);
+  const obj3 = { a: 3 };
+  pool.add(obj3);
+
+  const item1 = await pool.capture();
+  test.strictSame(item1, obj1);
+  const item2 = await pool.capture();
+  test.strictSame(item2, obj2);
+  const item3 = await pool.capture();
+  test.strictSame(item3, obj3);
+
+  pool.release(obj3);
+  const item4 = await pool.capture();
+  test.strictSame(item4, obj3);
+  test.end();
+});
+
 metatests.test('Pool: wait for release', async (test) => {
   const pool = new metautil.Pool();
-  const obj1 = {};
-  const obj2 = {};
+
+  const obj1 = { a: 1 };
   pool.add(obj1);
+  const obj2 = { a: 2 };
   pool.add(obj2);
+
   const item1 = await pool.capture();
   const item2 = await pool.capture();
+
   pool.capture().then((item3) => {
     test.strictSame(item3, obj2);
   });
   pool.capture().then((item4) => {
     test.strictSame(item4, obj1);
   });
+
   pool.release(item2);
   pool.release(item1);
   test.end();
@@ -69,19 +101,24 @@ metatests.test('Pool: wait for release', async (test) => {
 
 metatests.test('Pool: wait timeout', async (test) => {
   const pool = new metautil.Pool();
-  const obj1 = {};
-  const obj2 = {};
+
+  const obj1 = { a: 1 };
   pool.add(obj1);
+  const obj2 = { a: 2 };
   pool.add(obj2);
+
   const item1 = await pool.capture();
-  const item2 = await pool.capture();
   test.strictSame(item1, obj1);
+  const item2 = await pool.capture();
+  test.strictSame(item2, obj2);
+
   pool.capture().then((item3) => {
     test.strictSame(item3, obj2);
   });
   pool.capture().catch((err) => {
     test.strictSame(err.message, 'Error: Pool next item timeout');
   });
-  pool.release(item2);
+
+  pool.release(obj2);
   test.end();
 });
