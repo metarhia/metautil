7d5eb8ef0e673347d29639b8dde8c4c72456e9d6
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-07-21 23:54:40 +0300

    Move httpApiCall from impress (bus/request)
    PR-URL: https://github.com/metarhia/metautil/pull/174

--- test/network.js
+++ test/network.js
@@ -3,13 +3,14 @@
 const metatests = require('metatests');
 const metautil = require('..');
 
+const HOST = 'openexchangerates.org';
+const PATH = '/api/latest.json?app_id=';
+const API_KEY = '1f43ea96b1e343fe94333dd2b97a109d';
+const API_URL = `https://${HOST}/${PATH}${API_KEY}`;
+
 const getRate = async (currency) => {
-  const host = 'openexchangerates.org';
-  const path = '/api/latest.json?app_id=';
-  const key = '1f43ea96b1e343fe94333dd2b97a109d';
-  const url = `https://${host}/${path}${key}`;
   try {
-    const res = await metautil.fetch(url);
+    const res = await metautil.fetch(API_URL);
     const data = await res.json();
     const rate = data.rates[currency];
     return rate;
@@ -18,7 +19,7 @@ const getRate = async (currency) => {
   }
 };
 
-metatests.test('Fetch', async (test) => {
+metatests.test('Newtork: Fetch', async (test) => {
   const rate = await getRate('USD');
   test.strictSame(rate, 1);
   const method = 'POST';
@@ -35,7 +36,7 @@ metatests.test('Fetch', async (test) => {
   test.end();
 });
 
-metatests.test('receiveBody', async (test) => {
+metatests.test('Newtork: receiveBody', async (test) => {
   const value = Buffer.from('{ "a": 5 }');
   let done = false;
   const body = {
@@ -71,3 +72,14 @@ metatests.case(
     ],
   },
 );
+
+metatests.test('Newtork: httpApiCall', async (test) => {
+  const res = await metautil.httpApiCall(API_URL, { method: 'GET' });
+  test.strictSame(typeof res.disclaimer, 'string');
+  test.strictSame(typeof res.license, 'string');
+  test.strictSame(typeof res.timestamp, 'number');
+  test.strictSame(res.base, 'USD');
+  test.strictSame(typeof res.rates, 'object');
+  test.strictSame(typeof res.rates['UAH'], 'number');
+  test.end();
+});
