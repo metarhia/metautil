fd2cbb2eb63d0a9721e4eee56e3dbd1e366d63fb
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-08-25 17:53:29 +0300

    Add shuffle tests and typings

--- test/random.js
+++ test/random.js
@@ -25,6 +25,43 @@ metatests.case(
       [['a', 'b'], (result) => JSON.stringify(result.sort()) === '["a","b"]'],
       [[1, 'a', 3], (result) => JSON.stringify(result.sort()) === '[1,3,"a"]'],
       [[], (result) => JSON.stringify(result.sort()) === '[]'],
+      [
+        [1, 2, 3],
+        Math.random,
+        (result) => JSON.stringify(result.sort()) === '[1,2,3]',
+      ],
+      [
+        ['a', 'b'],
+        Math.random,
+        (result) => JSON.stringify(result.sort()) === '["a","b"]',
+      ],
+      [
+        [1, 'a', 3],
+        Math.random,
+        (result) => JSON.stringify(result.sort()) === '[1,3,"a"]',
+      ],
+      [[], Math.random, (result) => JSON.stringify(result.sort()) === '[]'],
+
+      [
+        [1, 2, 3],
+        metautil.cryptoRandom,
+        (result) => JSON.stringify(result.sort()) === '[1,2,3]',
+      ],
+      [
+        ['a', 'b'],
+        metautil.cryptoRandom,
+        (result) => JSON.stringify(result.sort()) === '["a","b"]',
+      ],
+      [
+        [1, 'a', 3],
+        metautil.cryptoRandom,
+        (result) => JSON.stringify(result.sort()) === '[1,3,"a"]',
+      ],
+      [
+        [],
+        metautil.cryptoRandom,
+        (result) => JSON.stringify(result.sort()) === '[]',
+      ],
     ],
   },
 );
