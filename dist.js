c50053c1f69947dd8ecfdc32e87881bce309ae8d
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-11-17 23:10:27 +0200

    Create browser entrypoint
    PR-URL: https://github.com/metarhia/metautil/pull/200

---
+++ dist.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  ...require('./lib/array.js'),
+  ...require('./lib/async.js'),
+  ...require('./lib/datetime.js'),
+  ...require('./lib/error.js'),
+  ...require('./lib/http.js'),
+  ...require('./lib/objects.js'),
+  ...require('./lib/pool.js'),
+  ...require('./lib/semaphore.js'),
+  ...require('./lib/strings.js'),
+  ...require('./lib/units.js'),
+};
