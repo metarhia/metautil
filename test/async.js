e40b765f9a3ac2e1b781a2a2e5d318a0654504cc
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2024-12-12 09:53:40 +0200

    Add more tests

--- test/async.js
+++ test/async.js
@@ -2,14 +2,10 @@
 
 const test = require('node:test');
 const assert = require('node:assert');
-const {
-  toBool,
-  timeout,
-  delay,
-  timeoutify,
-  throttle,
-  debounce,
-} = require('..');
+const metautil = require('..');
+const { toBool, timeout, delay } = metautil;
+const { timeoutify, throttle, debounce } = metautil;
+const { callbackify, asyncify, promisify } = metautil;
 
 test('Async: toBool', async () => {
   const success = await Promise.resolve('success').then(...toBool);
@@ -75,14 +71,17 @@ test('Async: timeoutify', async () => {
   }
 });
 
-test('Async: throttle', () => {
+test('Async: throttle', async () => {
+  const { promise, resolve } = Promise.withResolvers();
   let callCount = 0;
 
   const fn = (arg1, arg2, ...otherArgs) => {
     assert.strictEqual(arg1, 'someVal');
     assert.strictEqual(arg2, 4);
-    assert.strictEqual(otherArgs, []);
+    assert.deepEqual(otherArgs, []);
     callCount++;
+    assert.ok(callCount <= 2);
+    if (callCount === 2) resolve();
   };
 
   const throttledFn = throttle(1, fn, 'someVal', 4);
@@ -90,18 +89,23 @@ test('Async: throttle', () => {
   throttledFn();
   assert.strictEqual(callCount, 1);
   throttledFn();
+  assert.strictEqual(callCount, 1);
   throttledFn();
   assert.strictEqual(callCount, 1);
+  return promise;
 });
 
-test('Async: throttle merge args', () => {
+test('Async: throttle merge args', async () => {
+  const { promise, resolve } = Promise.withResolvers();
   let callCount = 0;
 
   const fn = (arg1, arg2, ...otherArgs) => {
     assert.strictEqual(arg1, 'someVal');
     assert.strictEqual(arg2, 4);
-    assert.strictEqual(otherArgs, ['str']);
+    assert.deepEqual(otherArgs, ['str']);
     callCount++;
+    assert.ok(callCount <= 2);
+    if (callCount === 2) resolve();
   };
 
   const throttledFn = throttle(1, fn, 'someVal', 4);
@@ -109,16 +113,21 @@ test('Async: throttle merge args', () => {
   throttledFn('str');
   assert.strictEqual(callCount, 1);
   throttledFn('str');
+  assert.strictEqual(callCount, 1);
   throttledFn('str');
   assert.strictEqual(callCount, 1);
+  return promise;
 });
 
-test('Async: throttle without arguments', () => {
+test('Async: throttle without arguments', async () => {
+  const { promise, resolve } = Promise.withResolvers();
   let callCount = 0;
 
   const fn = (...args) => {
-    assert.strictEqual(args, []);
+    assert.deepEqual(args, []);
     callCount++;
+    assert.ok(callCount <= 2);
+    if (callCount === 2) resolve();
   };
 
   const throttledFn = throttle(1, fn);
@@ -126,8 +135,10 @@ test('Async: throttle without arguments', () => {
   throttledFn();
   assert.strictEqual(callCount, 1);
   throttledFn();
+  assert.strictEqual(callCount, 1);
   throttledFn();
   assert.strictEqual(callCount, 1);
+  return promise;
 });
 
 test('Async: debounce', async () => {
@@ -137,14 +148,16 @@ test('Async: debounce', async () => {
   const fn = (arg1, arg2, ...otherArgs) => {
     assert.strictEqual(arg1, 'someVal');
     assert.strictEqual(arg2, 4);
-    assert.strictEqual(otherArgs, []);
+    assert.deepEqual(otherArgs, []);
     count++;
+    assert.strictEqual(count, 1);
     resolve();
   };
 
   const debouncedFn = debounce(1, fn, 'someVal', 4);
 
   debouncedFn();
+  assert.strictEqual(count, 0);
   debouncedFn();
   assert.strictEqual(count, 0);
   return promise;
@@ -155,15 +168,84 @@ test('Async: debounce without arguments', async () => {
   let count = 0;
 
   const fn = (...args) => {
-    assert.strictEqual(args, []);
+    assert.deepEqual(args, []);
     count++;
+    assert.strictEqual(count, 1);
     resolve();
   };
 
   const debouncedFn = debounce(1, fn);
 
   debouncedFn();
+  assert.strictEqual(count, 0);
   debouncedFn();
   assert.strictEqual(count, 0);
   return promise;
+});
+
+test('Callbackify: Promise to callback-last', async () => {
+  const { promise, resolve } = Promise.withResolvers();
+
+  const promiseReturning = () => Promise.resolve('result');
+  const asyncFn = callbackify(promiseReturning);
+
+  asyncFn((err, value) => {
+    assert.ifError(err);
+    assert.strictEqual(value, 'result');
+    resolve();
+  });
+
+  return promise;
+});
+
+test('Asyncify: sync function to callback-last', async () => {
+  const { promise, resolve } = Promise.withResolvers();
+
+  const fn = (par) => par;
+  const asyncFn = asyncify(fn);
+
+  asyncFn('result', (err, value) => {
+    if (err) assert.ifError(err);
+    assert.strictEqual(value, 'result');
+    resolve();
+  });
+
+  return promise;
+});
+
+test('Promisify: callback-last to Promise', async () => {
+  const id = 100;
+  const data = { key: 'value' };
+
+  const getDataAsync = (dataId, callback) => {
+    assert.strictEqual(dataId, id);
+    callback(null, data);
+  };
+
+  const getDataPromise = promisify(getDataAsync);
+
+  try {
+    const result = await getDataPromise(id);
+    assert.strictEqual(result, data);
+  } catch (err) {
+    assert.ifError(err);
+  }
+});
+
+test('Promisify: callback-last to Promise throw', async () => {
+  const id = 100;
+
+  const getDataAsync = (dataId, callback) => {
+    assert.strictEqual(dataId, id);
+    callback(new Error('Data not found'));
+  };
+
+  const getDataPromise = promisify(getDataAsync);
+
+  try {
+    const result = await getDataPromise(id);
+    assert.notOk(result);
+  } catch (err) {
+    assert.ok(err);
+  }
 });
