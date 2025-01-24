7ee1b60b2062e4e278cc6807f0f853b3ad6be7f7
GOODLUCKMISTER <paet14@gmail.com>
2025-01-24 23:58:20 +0200

    added more tests and fixes

--- test/events.js
+++ test/events.js
@@ -60,74 +60,144 @@ test('EventEmitter', async () => {
   ee.clear();
   assert.strictEqual(ee.listenerCount('name3'), 0);
 
-  const iterator = ee.toIterator('name4');
-  process.nextTick(() => {
-    ee.emit('name4', 'foo', 'bar', 12);
-    ee.emit('name4', 'banana');
-  });
-  setTimeout(() => ee.emit('name4', 0), 0);
-  setTimeout(() => ee.emit('name4', 1, 11, 'test'), 0);
-  setTimeout(() => ee.emit('name4', 2), 0);
-  setTimeout(() => ee.emit('name4', 'stop'), 0);
-  const iteratorExpect = [
-    ['foo', 'bar', 12],
-    ['banana'],
-    [0],
-    [1, 11, 'test'],
-    [2],
-    ['stop'],
-  ];
-  for await (const event of iterator) {
-    const current = iteratorExpect.shift();
-    assert.deepStrictEqual(current, event);
-    if (!iteratorExpect.length) {
-      break;
-    }
-  }
-  assert.strictEqual(ee.listenerCount('name4'), 0);
-  assert.strictEqual(ee.listenerCount('error'), 0);
-
-  const iteratorWithError = ee.toIterator('bang');
-  const expectedError = new Error('Big bang');
-  process.nextTick(() => {
-    ee.emit('error', expectedError);
-  });
-  let looped = false;
-  let thrown = false;
-  try {
-    // eslint-disable-next-line no-unused-vars
-    for await (const event of iteratorWithError) {
-      looped = true;
-    }
-  } catch (err) {
-    thrown = true;
-    assert.strictEqual(err, expectedError);
-  }
-  assert.strictEqual(thrown, true);
-  assert.strictEqual(looped, false);
-  assert.strictEqual(ee.listenerCount('bang'), 0);
-  assert.strictEqual(ee.listenerCount('error'), 0);
-
-  const _err = new Error('kaboom');
-
-  process.nextTick(() => {
-    ee.emit('foo', 42);
-  });
-
-  try {
-    for await (const event of ee.toIterator('foo')) {
-      assert.deepStrictEqual(event, [42]);
-      throw _err;
-    }
-  } catch (err) {
-    assert.strictEqual(err, _err);
-  }
-
-  assert.strictEqual(ee.listenerCount('foo'), 0);
-  assert.strictEqual(ee.listenerCount('error'), 0);
-
   const emitExpect = ['await emit 1', 'await emit 2', 'await emit 3'];
   emitExpect.forEach((e) => ee.on('name5', () => e));
   const emitResult = await ee.emit('name5');
   assert.deepStrictEqual(emitResult, emitExpect);
+});
+
+test('EventIterator', async (testCase) => {
+  await testCase.test('base', async () => {
+    const ee = new metautil.EventEmitter();
+    const iterator = ee.toIterator('name4');
+    process.nextTick(async () => {
+      await ee.emit('name4', 'foo', 'bar', 12);
+      await ee.emit('name4', 'banana');
+      await ee.emit('name4', 'nana');
+    });
+    setTimeout(() => ee.emit('name4', 0), 0);
+    setTimeout(() => ee.emit('name4', 1, 11, 'test'), 0);
+    setTimeout(() => ee.emit('name4', 2), 0);
+    setTimeout(() => ee.emit('name4', 'stop'), 0);
+    const iteratorExpect = [
+      ['foo', 'bar', 12],
+      ['banana'],
+      ['nana'],
+      [0],
+      [1, 11, 'test'],
+      [2],
+      ['stop'],
+    ];
+    for await (const event of iterator) {
+      const current = iteratorExpect.shift();
+      assert.deepStrictEqual(current, event);
+      if (!iteratorExpect.length) {
+        break;
+      }
+    }
+    assert.strictEqual(ee.listenerCount('name4'), 0);
+    assert.strictEqual(ee.listenerCount('error'), 0);
+  });
+  await testCase.test('error', async () => {
+    const ee = new metautil.EventEmitter();
+    const iteratorWithError = ee.toIterator('bang');
+    const expectedError = new Error('Big bang');
+    process.nextTick(() => {
+      ee.emit('error', expectedError);
+    });
+    let looped = false;
+    let thrown = false;
+    try {
+      // eslint-disable-next-line no-unused-vars
+      for await (const event of iteratorWithError) {
+        looped = true;
+      }
+    } catch (err) {
+      thrown = true;
+      assert.strictEqual(err, expectedError);
+    }
+    assert.strictEqual(thrown, true);
+    assert.strictEqual(looped, false);
+  });
+  await testCase.test('errorDelayed', async () => {
+    const ee = new metautil.EventEmitter();
+    const _err = new Error('kaboom');
+    process.nextTick(async () => {
+      await ee.emit('foo', 42);
+      await ee.emit('error', _err);
+    });
+
+    const iterable = ee.toIterator('foo');
+    const expected = [[42]];
+    let thrown = false;
+
+    try {
+      for await (const event of iterable) {
+        const current = expected.shift();
+        assert.deepStrictEqual(current, event);
+      }
+    } catch (err) {
+      thrown = true;
+      assert.strictEqual(err, _err);
+    }
+    assert.strictEqual(thrown, true);
+    assert.strictEqual(ee.listenerCount('foo'), 0);
+    assert.strictEqual(ee.listenerCount('error'), 0);
+  });
+
+  await testCase.test('throwInLoop', async () => {
+    const ee = new metautil.EventEmitter();
+    const _err = new Error('kaboom');
+
+    process.nextTick(() => {
+      ee.emit('foo', 42);
+    });
+
+    try {
+      for await (const event of ee.toIterator('foo')) {
+        assert.deepStrictEqual(event, [42]);
+        throw _err;
+      }
+    } catch (err) {
+      assert.strictEqual(err, _err);
+    }
+
+    assert.strictEqual(ee.listenerCount('foo'), 0);
+    assert.strictEqual(ee.listenerCount('error'), 0);
+  });
+
+  await testCase.test('next', async () => {
+    const ee = new metautil.EventEmitter();
+    const iterable = ee.toIterator('foo');
+
+    process.nextTick(async () => {
+      await ee.emit('foo', 'bar');
+      await ee.emit('foo', 42);
+      iterable.return();
+    });
+
+    const first = await iterable.next();
+    const second = await iterable.next();
+    const third = await iterable.next();
+    const results = [first, second, third];
+
+    assert.deepStrictEqual(results, [
+      {
+        value: ['bar'],
+        done: false,
+      },
+      {
+        value: [42],
+        done: false,
+      },
+      {
+        value: undefined,
+        done: true,
+      },
+    ]);
+    assert.deepStrictEqual(await iterable.next(), {
+      value: undefined,
+      done: true,
+    });
+  });
 });
